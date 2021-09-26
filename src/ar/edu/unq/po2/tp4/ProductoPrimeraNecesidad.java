002f60c1c5c579b6cccb3b29442fbdefcf52457a
unknown <valen.ferreyra@outlook.com>
2021-09-26 15:28:04 -0300

    refactorizacion del ProductoPrimeraNecesidad para poder aplicar descuento a partir de parametro

--- src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
+++ src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
@@ -1,22 +1,28 @@
 package ar.edu.unq.po2.tp4;
 
 public class ProductoPrimeraNecesidad extends Producto {
+	
+	private int descuento;
 
-	public ProductoPrimeraNecesidad(String nombre, Double precio) {
+	public ProductoPrimeraNecesidad(String nombre, Double precio, int descuento) {
 		super(nombre, precio);
+		this.setDescuento(descuento);
 	}
 	
-	public ProductoPrimeraNecesidad(String nombre, Double precio, boolean esPrecioCuidado) {
+	public ProductoPrimeraNecesidad(String nombre, Double precio, boolean esPrecioCuidado, int descuento) {
 		super(nombre, precio, esPrecioCuidado);
+		this.setDescuento(descuento);
+	}
+	
+	private void setDescuento(int descuento) {
+		this.descuento = descuento;
 	}
 	
 	@Override
 	public double getPrecio() {
-		return this.aplicarDescuento(super.getPrecio());
-	}
-	
-	private double aplicarDescuento(double precio) {
-		return precio * 0.9;
+		double descuentoTotal = (super.getPrecio() * this.descuento) / 100;
+		
+		return (super.getPrecio() - descuentoTotal);
 	}
 
 }
--- test/tp4/testing/ProductoPrimeraNecesidadTest.java
+++ test/tp4/testing/ProductoPrimeraNecesidadTest.java
@@ -10,14 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ProductoPrimeraNecesidadTest {
 
 	private ProductoPrimeraNecesidad leche;
+	private ProductoPrimeraNecesidad arroz;
 	
 	@BeforeEach
 	public void setUp() {
-		leche = new ProductoPrimeraNecesidad("Leche", 8d, false);
+		leche = new ProductoPrimeraNecesidad("Leche", 100.0, false, 11);
+		arroz = new ProductoPrimeraNecesidad("Leche", 100.0, false, 9);
 	}
 	
 	@Test
 	public void testCalcularPrecio() {
-		assertEquals(Double.valueOf(7.2), leche.getPrecio());
+		assertEquals(Double.valueOf(89.0), leche.getPrecio());
+		assertEquals(Double.valueOf(91.0), arroz.getPrecio());
 	}
 }
